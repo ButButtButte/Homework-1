cfbfe3ebc2ac7631086a7cde35c3fadef77629bd
butbuttbutte <dennisrobinson1996@gmail.com>
2024-02-02 15:42:40 -0600

    Initial Commit

---
+++ Homework 1/Homework_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_1App.swift
+//  Homework 1
+//
+//  Created by PHYS 440 Dennis on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Homework_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
