b065aa580ca943b3b5a1231b8fcfae83efa59b7d
butbuttbutte <dennisrobinson1996@gmail.com>
2024-02-02 18:04:11 -0600

    PLS

--- Homework 1/ContentView.swift
+++ Homework 1/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sphereRadius = ""
+    @State private var sphereVolume = ""
+    @State private var sphereSurfaceArea = ""
+    @State private var boundingBoxVolume = ""
+    @State private var boundingBoxSurfaceArea = ""
+    
     var body: some View {
         VStack {
+            
+            
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Goodbye, world!")
         }
         .padding()
     }
--- Homework 1/Parameters.swift
+++ Homework 1/Parameters.swift
@@ -5,4 +5,49 @@
 //  Created by PHYS 440 Dennis on 2/2/24.
 //
 
-import Foundation
+import SwiftUI
+import Observation
+
+@Observable class Sphere {
+    var sphereRadius = "0.0"
+    var sphereVolume = "0.0"
+    var sphereSurfaceArea = "0.0"
+    var boundingBoxVolume = "0.0"
+    var boundingBoxSurfaceArea = "0.0"
+   
+    var sphereRadiusDouble = 0.0
+    var sphereVolumeDouble = 0.0
+    var sphereSurfaceAreaDouble = 0.0
+    var boundingBoxVolumeDouble = 0.0
+    var boundingBoxSurfaceAreaDouble = 0.0
+    
+    func initWithRadius(radius: Double) -> Bool{
+        sphereRadiusDouble = radius
+        
+        return true
+    }
+    
+    func calculate() {
+        let theSphereRadius = Double(sphereRadius) ?? 0
+        let theSphereVolume = (4.0/3.0) * Double.pi * theSphereRadius * theSphereRadius * theSphereRadius
+        let theSphereSA = 4.0 * Double.pi * theSphereRadius * theSphereRadius
+        
+        let theBoundingBoxVolume = 2.0 * theSphereRadius * theSphereRadius * theSphereRadius
+        let theBoundingBoxSA = 6.0 * 2.0 * theSphereRadius
+        
+        print(sphereRadius)
+        
+        sphereVolume = String(theSphereVolume)
+        sphereSurfaceArea = String(theSphereSA)
+        boundingBoxVolume = String(theBoundingBoxSA)
+        boundingBoxSurfaceArea = String(theBoundingBoxSA)
+        
+        sphereVolumeDouble = theSphereVolume
+        sphereSurfaceAreaDouble = theSphereSA
+        boundingBoxVolumeDouble = theBoundingBoxVolume
+        boundingBoxSurfaceAreaDouble = theBoundingBoxSA
+        
+        
+        
+    }
+}
